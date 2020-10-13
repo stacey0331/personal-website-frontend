b555ca4e135b4daa6843e7cf89882d219a0fc075
stacey0331 <staceyleetsaihsun@gmail.com>
2020-10-14 00:11:24 +0800

    Heroku test

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 8000;
 
-app.use(express.static(publicPath));
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
