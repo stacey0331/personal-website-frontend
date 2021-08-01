080863ae041f7693dad31524b224bee9989f3de9
Stacey <licaixun@onchain.com>
2021-08-01 23:03:30 +0800

    heroku compile failed fix attemp

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static('client/build'));
+app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
